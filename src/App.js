131afd791dbc3f60b5268403a80299c4b8d2abdb
Sabin <sabintheworld@gmail.com>
2019-09-22 02:04:10 -0700

    Added sub-app to showcase shared state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss'
 import PointsUseState from './components/PointsUseState'
 import PointsUseReducer from './components/PointsUseReducer'
+import StateApp from './StateApp'
 
 function App() {
   return (
     <div className="App">
       <PointsUseState />
       <PointsUseReducer />
+      <StateApp />
     </div>
   );
 }
